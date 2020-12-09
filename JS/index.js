4bb96daa192b68c03765068acc2f31e0fef1cc81
deebridgewater <67491060+deebridgewater@users.noreply.github.com>
2020-12-08 18:29:11 -0800

    update

--- JS/index.js
+++ JS/index.js
@@ -6,3 +6,30 @@ function fade() {
     $("flexContainer").ready(function(){
         $("container");
     })
+
+// Hides class 'accessibility' when page loads
+    $(document).ready(function() {
+        $(".accessibility").hide();
+        $(".collaboration").hide();
+    });   
+
+    // Fades div with class '.accessibility' in when user has scrolled down more 550 pixels
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 350) {
+      $(".accessibility").fadeIn();
+      $(".collaboration").fadeIn();
+      $(".uxUi").fadeIn();
+      $(".userResearch").fadeIn();
+      $(".interactionDesign").fadeIn();
+      $(".appliedBehavior").fadeIn();
+      $(".figma").fadeIn();
+      $(".invision").fadeIn();
+      $(".html").fadeIn();
+      $(".collaboration").fadeIn();
+      // $(".collaboration").fadeIn();
+    } else {
+      // $(".accessibility").fadeOut();
+      // $(".collaboration").fadeOut();
+    }
+  });
